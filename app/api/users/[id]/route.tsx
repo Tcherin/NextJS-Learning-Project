d95d206df110f67e006fe355da981583c89543ab
Richard Dalrymple <117475164+Tcherin@users.noreply.github.com>
2024-01-29 21:35:50 +0000

    added prisma client

---
+++ app/api/users/[id]/route.tsx
@@ -0,0 +1,8 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export function GET(request: NextRequest, { params }: { params: {id: number} }) {
+    if (params.id > 10)
+        return NextResponse.json({error: 'User not found'});
+
+    return NextResponse.json({ id:1, name: "Richard" });
+}
---
+++ app/api/users/route.tsx
@@ -0,0 +1,15 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export function GET(request: NextRequest) {
+    return NextResponse.json([
+        {id: '1', name: "Richard" },
+        {id: '2', name: "John" },
+    ]);
+}
+
+export async function POST(request: NextRequest) {
+    const body = await request.json();
+    if(!body.name)
+        return NextResponse.json({error: 'Name is required'})
+    return NextResponse.json({id: 1, name: body.name});
+}
